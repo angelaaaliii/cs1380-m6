bbce2aa9383948a2cfe0f850830aa1eba9856aba
angelaaaliii <angelali2021@gmail.com>
2025-01-26 19:17:12 +0000

    student tests implemented

--- non-distribution/t/ts/s_test_combine.sh
+++ non-distribution/t/ts/s_test_combine.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 # This is a student test
 
+R_FOLDER=${R_FOLDER:-}
+
+cd "$(dirname "$0")/..$R_FOLDER" || exit 1
+
+DIFF=${DIFF:-diff}
+
+if $DIFF <(cat /usr/src/app/non-distribution/t/ts/d/d14.txt | /usr/src/app/non-distribution/c/combine.sh | sed 's/\t*$//' | sed 's/\s/ /g' | sort | uniq) <(cat /usr/src/app/non-distribution/t/ts/d/d15.txt | sed 's/\t*$//' | sed 's/\s/ /g' | sort | uniq) >&2;
+then
+    echo "$0 success: ngrams are identical"
+    exit 0
+else
+    echo "$0 failure: ngrams are not identical"
+    exit 1
+fi
--- non-distribution/t/ts/s_test_stem.sh
+++ non-distribution/t/ts/s_test_stem.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
 # This is a student test
+
+T_FOLDER=${T_FOLDER:-t}
+TS_FOLDER=${TS_FOLDER:-ts}
+R_FOLDER=${R_FOLDER:-}
+
+cd "$(dirname "$0")/..$R_FOLDER" || exit 1
+
+DIFF=${DIFF:-diff}
+
+
+if $DIFF <(cat "$TS_FOLDER"/d/d12.txt | /../../c/stem.js | sort) <(sort "$TS_FOLDER"/d/d13.txt) >&2;
+then
+    echo "$0 success: stemmed words are identical"
+    exit 0
+else
+    echo "$0 failure: stemmed words are not identical"
+    exit 1
+fi
