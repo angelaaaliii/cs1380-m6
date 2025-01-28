c4d51cee10a9c04d74e7cc7ab7d83919dc8df4d1
angelaaaliii <angelali2021@gmail.com>
2025-01-28 03:02:23 +0000

    lab part 1 done

---
+++ non-distribution/c/invert.js
@@ -0,0 +1,17 @@
+const baseURL = process.argv[2];
+const terms = process.argv[3].split('\n');
+const tracker = {};
+for (let i = 0; i < terms.length; i++) {
+  const term = terms[i];
+  if (term in tracker) {
+    tracker[term]+= 1;
+  } else {
+    tracker[term] = 1;
+  }
+}
+
+let outputStr = '';
+for (const k in tracker) {
+  outputStr += k + ' | ' + tracker[k] + ' | ' + baseURL + '\n';
+}
+console.log(outputStr.substring(0, outputStr.length-1));
--- non-distribution/c/invert.sh
+++ non-distribution/c/invert.sh
@@ -4,4 +4,8 @@
 
 # Usage: ./invert.sh url < n-grams
 
-grep -v $'\t+$' | sort | uniq -c | awk '{print $2,$3,$4,"|",$1,"|"}' | sed 's/\s\+/ /g' | sort | sed "s|$| $1|"
+#grep -v $'\t+$' | sort | uniq -c | awk '{print $2,$3,$4,"|",$1,"|"}' | sed 's/\s\+/ /g' | sort | sed "s|$| $1|"
+
+INPUT=$(cat)
+
+node c/invert.js "$1" "$INPUT"
