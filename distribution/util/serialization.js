00eb3f9124e67066d08a83a04a0ebcdbd93a62ca
angelaaaliii <angelali2021@gmail.com>
2025-01-31 17:29:15 +0000

    T2 done M1

--- distribution/util/serialization.js
+++ distribution/util/serialization.js
@@ -15,10 +15,38 @@
 
 
 function serialize(object) {
+  let serialized_obj;
+  if (typeof(object) == "number") {
+    serialized_obj = {type: "number", value: object.toString()};
+  } else if (typeof(object) == "string") {
+    serialized_obj = {type: "string", value: object.toString()};
+  } else if (typeof(object) == "boolean") {
+    serialized_obj = {type: "boolean", value: object.toString()};
+  } else if (object == null) {
+    serialized_obj = {type: "null", value: object.toString()};
+  } else if (object == undefined) {
+    serialized_obj = {type: "undefined", value: object.toString()};
+  } 
+  // else if (typeof(object) == "function") {
+  //   serialized_obj = {type: "function", value: {inputs: [], body: }}
+  // }
+  return serialized_obj.toString();
 }
 
 
 function deserialize(string) {
+  const json_obj = JSON.parse(string);
+  if (json_obj[type] == "number") {
+    return Number(json_obj[value]);
+  } else if (json_obj[type] == "string") {
+    return json_obj[value];
+  } else if (json_obj[type] == "boolean") {
+    return Boolean(json_obj[value]);
+  } else if (json_obj[type] == "null") {
+    return null;
+  } else if (json_obj[type] == "undefined") {
+    return undefined;
+  }
 }
 
 module.exports = {
