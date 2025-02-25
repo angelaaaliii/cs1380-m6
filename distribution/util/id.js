f5bceeefa1acea4ef8654cc43e646b8db0e11c5d
angelaaaliii <angelali2021@gmail.com>
2025-02-25 18:15:37 +0000

    all store passing

--- distribution/util/id.js
+++ distribution/util/id.js
@@ -59,6 +59,24 @@ function consistentHash(kid, nids) {
 
 
 function rendezvousHash(kid, nids) {
+  /*
+  In our setting, given a KID and a list of NIDs, utils.rendezvousHash needs to:
+
+create a new list by concatenating each NID with the KID (any concatenation order is correct, but the tests expect you to do kid + nid.)
+hash these values (e.g., sha256) and then convert them to a numerical representation,
+sort the resulting values and pick the maximum, and
+convert the chosen element back into an ID and return i
+*/
+  let maxVal = -1;
+  let maxNid;
+  for (let nid of nids) {
+    const hashNum = idToNum(getID(kid + nid));
+    if (hashNum > maxVal) {
+      maxVal = hashNum;
+      maxNid = nid;
+    }
+  }
+  return maxNid;
 }
 
 module.exports = {
