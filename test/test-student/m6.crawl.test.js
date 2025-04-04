ffd37afd2b5734204ad57f7726d526da81a63641
angelaaaliii <angelali2021@gmail.com>
2025-04-03 23:00:59 -0400

    added 3rd arg to mapper, modified getURLs, crawling sorta working - VERY slow

--- test/test-student/m6.crawl.test.js
+++ test/test-student/m6.crawl.test.js
@@ -4,9 +4,11 @@
 
     Imporant: Do not modify any of the test headers (i.e., the test('header', ...) part). Doing so will result in grading penalties.
 */
+jest.setTimeout(1000000);
 const distribution = require('../../config.js');
 const id = distribution.util.id;
-const {execSync} = require('child_process');
+const fs = require('fs');
+// const {execSync} = require('child_process');
 
 const crawlGroup = {};
 
@@ -19,170 +21,51 @@ const n1 = {ip: '127.0.0.1', port: 7110};
 const n2 = {ip: '127.0.0.1', port: 7111};
 const n3 = {ip: '127.0.0.1', port: 7112};
 
-test('EXEC SYNC', (done) => {
-    // download contents of URL (mocking this with corpus mapping)
-    const key = 'httpsenwikipediaorgwikiWikipediaAprilFools';
-    const value = {"original_url":"https://en.wikipedia.org/wiki/Wikipedia:April_Fools"};
-
-    const original_url = value['original_url'];
-    const rawPgContent = execSync(`curl -skL "${original_url}"`, {encoding: 'utf-8'}).toString().trim();
-    let urls = execSync(`echo ${JSON.stringify(rawPgContent)} | ./non-distribution/c/getURLs.js "${key}"`, { encoding: 'utf-8' }).toString().trim();
-    urls = urls.split('\n');
-    let pageText = rawPgContent;
-    if (!('value' in value)) {
-      console.log("HERE");
-      pageText = execSync(`echo ${JSON.stringify(rawPgContent)} | ./non-distribution/c/getText.js`, {encoding: 'utf-8'}).toString().trim();
-      value['value'] = pageText;
-    }
-
-    let res = [];
-
-    const inputKV = {};
-    inputKV[original_url] = value;
-    res.push(inputKV);
-
-    for (let url of urls) {
-      const out = {};
-      out[url] = {'original_url': url}; // downloading content of next link
-      res.push(out);
-    }
-    console.log("TEST 1 res = ", res);
-    fs.appendFileSync('test_debug.log', 'This is a test log entry.\n', 'utf8');
-
-});
-
 test('(15 pts) add support for iterative map-reduce', (done) => {
-  // TODO: seen/visited tracker
-  const mapper = (key, value) => {
-    // const {execSync} = require('child_process');
-    // const fs = require('fs');
-
-    // throw new Error("Mapper function is running!");
-
-    // download contents of URL (mocking this with corpus mapping)
-    const original_url = value['original_url'];
-    const rawPgContent = execSync(`curl -skL "${original_url}"`, {encoding: 'utf-8'}).toString().trim();
-    let urls = execSync(`echo ${JSON.stringify(rawPgContent)} | ./non-distribution/c/getURLs.js "${key}"`, { encoding: 'utf-8' }).toString().trim();
-    urls = urls.split('\n');
-
-    let pageText = rawPgContent;
-    if (!('value' in value)) {
-      pageText = execSync(`echo ${JSON.stringify(rawPgContent)} | ./non-distribution/c/getText.js`, {encoding: 'utf-8'}).toString().trim();
-      value['value'] = pageText;
-    }
-
-    let res = [];
-
-    const inputKV = {};
-    inputKV[original_url] = value;
-    res.push(inputKV);
-
-    for (let url of urls) {
-      const out = {};
-      out[url] = {'original_url': url}; // downloading content of next link
-      res.push(out);
-    }
-    return res;
-  };
-  
-  // Reduce function: remove duplicates
-  const reducer = (key, values) => {
-    const res = {};
-    const mySet = new Set(values);
-    if (mySet.size == 1) {
-      v = values[0];
-    } else {
-      for (const item of mySet) {
-        if (item != null) {
-          v = item;
-        }
-      }
-    }
-    res[key] = v;
-    return res;
-  };
-
-  const dataset = [
-    {"https://en.wikipedia.org/wiki/Wikipedia:April_Fools": {"original_url": "https://en.wikipedia.org/wiki/Wikipedia:April_Fools"}},
-    {"https://en.wikipedia.org/wiki/Wikipedia:April_Fools/April_Fools%27_Day_2022": {"original_url": "https://en.wikipedia.org/wiki/Wikipedia:April_Fools/April_Fools%27_Day_2022"}}
-  ];
-  
-    const doMapReduce = (cb) => {
-      distribution.crawl.store.get(null, (e, v) => {
-  
-        distribution.crawl.mr.exec({keys: v, map: mapper, reduce: reducer, rounds: 1, out: "CRAWL_TEST"}, (e, v) => {
-          
-          console.log("FINAL RES", v);
-          done();
-  
-        });
-      });
-    };
-  
-    let cntr = 0;
-  
-    // Send the dataset to the cluster
-    dataset.forEach((o) => {
-      const key = Object.keys(o)[0];
-      const value = o[key];
-      distribution.crawl.store.put(value, key, (e, v) => {
-        cntr++;
-        // Once the dataset is in place, run the map reduce
-        if (cntr === dataset.length) {
-          doMapReduce();
-        }
-      });
-    });
-});
-
-test.only('(15 pts) add support for iterative map-reduce', (done) => {
-  // TODO: seen/visited tracker
-  const mapper = (key, value) => {
-    console.log("IN MAPPER");
+  const mapper = (key, value, execArg) => {
     const original_url = value['original_url'];
     try {
-      const rawPgContent = execSync(`curl -skL "${original_url}"`, {encoding: 'utf-8'}).toString().trim();
-      let urls = execSync(`echo ${JSON.stringify(rawPgContent)} | ./non-distribution/c/getURLs.js "${key}"`, { encoding: 'utf-8' }).toString().trim();
-      urls = urls.split('\n');
-
-      let pageText = rawPgContent;
-      if (!('value' in value)) {
-        pageText = execSync(`echo ${JSON.stringify(rawPgContent)} | ./non-distribution/c/getText.js`, {encoding: 'utf-8'}).toString().trim();
-        value['value'] = pageText;
+      if ('page_text' in value) {
+        return [];
       }
+
+      const rawPgContent = execArg(`curl -skL --compressed "${original_url}"`, {encoding: 'utf-8'}).toString().trim();
+      let urls = execArg(`echo ${JSON.stringify(rawPgContent)} | ./non-distribution/c/getURLs.js "https://en.wikipedia.org"`, { encoding: 'utf-8' }).toString();
+      urls = urls.split('\n');
+      pageText = execArg(`echo ${JSON.stringify(rawPgContent)} | ./non-distribution/c/getText.js`, {encoding: 'utf-8'}).toString().trim();
+      value['page_text'] = pageText;
+    
 
       let res = [];
 
       const inputKV = {};
       inputKV[original_url] = value;
       res.push(inputKV);
-
       for (let url of urls) {
+        if (url == '') {
+          continue;
+        }
         const out = {};
-        out[url] = {'original_url': url}; // downloading content of next link
+        out[url] = {'original_url': url}; 
         res.push(out);
       }
       return res;
     }
     catch (e) {
-      console.error(e, e.message);
+      return [];
     }
   };
   
 
   const reducer = (key, values) => {
     const res = {};
-    const mySet = new Set(values);
-    if (mySet.size == 1) {
-      v = values[0];
-    } else {
-      for (const item of mySet) {
-        if (item != null) {
-          v = item;
-        }
+    for (const item of values) {
+      if (Object.keys(item).length == 2) {
+        res[key] = item;
+        return res;
       }
     }
-    res[key] = v;
+    res[key] = values[0];
     return res;
   };
 
@@ -205,10 +88,17 @@ test.only('(15 pts) add support for iterative map-reduce', (done) => {
     const doMapReduce = (cb) => {
       distribution.crawl.store.get(null, (e, v) => {
   
-        distribution.crawl.mr.exec({keys: v, map: mapper, reduce: reducer, rounds: 3}, (e, v) => {
+        distribution.crawl.mr.exec({keys: v, map: mapper, reduce: reducer, rounds: 2, out: "CRAWL_TEST"}, (e, v) => {
           try {
-            console.log(v);
-            expect(v).toEqual(expect.arrayContaining(expected));
+            // const url = "httpscommonswikimediaorgwikiWikipediaAprilFools";
+            const url = "httpsenwikipediaorgwikiWikipediaAprilFoolsAprilFools27Day2004";
+            distribution["CRAWL_TEST"].store.get(url, (e, v) => {
+              console.log(v);
+              expect(e).toBe(null);
+              expect(v.original_url).toBeDefined();
+              expect(v.page_text).toBeDefined();
+              done();
+            });
             done();
           } catch (e) {
             done(e);
