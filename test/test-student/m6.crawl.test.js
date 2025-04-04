e1847d0249ad133aecb509aa7971adf1eb84c20b
angelaaaliii <angelali2021@gmail.com>
2025-04-04 13:37:09 -0400

    crawl not working still, before implementing changes discussed

--- test/test-student/m6.crawl.test.js
+++ test/test-student/m6.crawl.test.js
@@ -23,60 +23,47 @@ const n3 = {ip: '127.0.0.1', port: 7112};
 const n4 = {ip: '127.0.0.1', port: 7113};
 const n5 = {ip: '127.0.0.1', port: 7114};
 
-test('(15 pts) add support for iterative map-reduce', (done) => {
-    try {
-      const filepath = "visited.txt";
-      const found = execSync(`grep -q "abc.txt" "${filepath}"`, {encoding: 'utf-8'}).toString();
-      expect(found).toBe("");
-      done();
-    } catch (e) {
-      console.error(e);
-      done();
-    }
-});
-
 test.only('(15 pts) add support for iterative map-reduce', (done) => {
   const mapper = (key, value, execArg, fs) => {
     const original_url = value['original_url'];
 
       const filepath = "visited.txt";
       try {
-        const found = execArg(`grep -q "${original_url}" "${filepath}"`, {encoding: 'utf-8'}).toString();
+        const found = execArg(`grep -q "${original_url}" "visited.txt"`, {encoding: 'utf-8'}).toString();
         // found
         return [];
       } catch (e) {
+        // not found, have not seen url before
         const rawPgContent = execArg(`curl -skL --compressed "${original_url}"`, {encoding: 'utf-8'}).toString().trim();
-        let urls = execArg(`echo ${JSON.stringify(rawPgContent)} | ./non-distribution/c/getURLs.js "https://en.wikipedia.org"`, { encoding: 'utf-8' }).toString();
-        urls = urls.split('\n');
-        if (!('page_text' in value)) {
-          const pageText = execArg(`echo ${JSON.stringify(rawPgContent)} | ./non-distribution/c/getText.js`, {encoding: 'utf-8'}).toString().trim();
-          value['page_text'] = pageText;
-        }
-
-        let res = [];
+        // let urls = execArg(`echo ${JSON.stringify(rawPgContent)} | ./non-distribution/c/getURLs.js "https://en.wikipedia.org"`, { encoding: 'utf-8' }).toString();
+        // urls = urls.split('\n');
+        // if (!('page_text' in value)) {
+        const pageText = execArg(`echo ${JSON.stringify(rawPgContent)} | ./non-distribution/c/getText.js`, {encoding: 'utf-8'}).toString().trim();
+        value['page_text'] = pageText;
+        // }
 
         const inputKV = {};
         inputKV[original_url] = value;
-        res.push(inputKV);
-        for (let url of urls) {
-          if (url == '') {
-            continue;
-          }
+        const res = [inputKV];
+        fs(filepath, original_url + "\n");
+        // for (let url of urls) {
+        //   if (url == '') {
+        //     continue;
+        //   }
 
-          try {
-            const found = execArg(`grep -q "${original_url}" "${filepath}"`, {encoding: 'utf-8'}).toString();
-            continue;
-          }
-          catch (e) {
-            const out = {};
-            const rawPgContent = execArg(`curl -skL --compressed "${url}"`, {encoding: 'utf-8'}).toString().trim();
-            pageText = execArg(`echo ${JSON.stringify(rawPgContent)} | ./non-distribution/c/getText.js`, {encoding: 'utf-8'}).toString().trim();
-            out[url] = {'original_url': url, 'page_text': pageText}; 
-            res.push(out);
-          }
-        }
-        fs(filepath, value.original_url + "\n");
-        console.log("RETURN RES", urls, res);
+        //   try {
+        //     const found = execArg(`grep -q "${original_url}" "${filepath}"`, {encoding: 'utf-8'}).toString();
+        //     continue;
+        //   }
+        //   catch (e) {
+        //     const out = {};
+        //     const rawPgContent = execArg(`curl -skL --compressed "${url}"`, {encoding: 'utf-8'}).toString().trim();
+        //     pageText = execArg(`echo ${JSON.stringify(rawPgContent)} | ./non-distribution/c/getText.js`, {encoding: 'utf-8'}).toString().trim();
+        //     out[url] = {'original_url': url, 'page_text': pageText}; 
+        //     res.push(out);
+        //   }
+        // }
+        // console.log("RETURN RES", urls, res);
         return res;
       }
   };
@@ -84,6 +71,8 @@ test.only('(15 pts) add support for iterative map-reduce', (done) => {
 
   const reducer = (key, values) => {
     const res = {};
+    const original_url = values[0].original_url;
+    
     for (const item of values) {
       if (Object.keys(item).length == 2) {
         res[key] = item;
@@ -204,3 +193,6 @@ afterAll((done) => {
     });
   });
 });
+
+// TODO: mapper and reducer split
+// no longer need reduce out gid to change to overall gid
