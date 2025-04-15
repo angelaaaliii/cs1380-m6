5ad45e90cf31fc4da84612c6368f7166b26046d7
Vivian Lu <78044870+viv-lu@users.noreply.github.com>
2025-04-14 20:48:33 -0400

    deploy test - add log statement to mapWrapper

--- test/test-student/m6.crawl.test.js
+++ test/test-student/m6.crawl.test.js
@@ -19,16 +19,20 @@ const crawlGroup = {};
 */
 let localServer = null;
 
-const n1 = {ip: '127.0.0.1', port: 7111};
-const n2 = {ip: '127.0.0.1', port: 7112};
-const n3 = {ip: '127.0.0.1', port: 7113};
-const n4 = {ip: '127.0.0.1', port: 7114};
-const n5 = {ip: '127.0.0.1', port: 7115};
-const n6 = {ip: '127.0.0.1', port: 7116};
+// const n1 = {ip: '127.0.0.1', port: 7111};
+// const n2 = {ip: '127.0.0.1', port: 7112};
+// const n3 = {ip: '127.0.0.1', port: 7113};
+// const n4 = {ip: '127.0.0.1', port: 7114};
+// const n5 = {ip: '127.0.0.1', port: 7115};
+// const n6 = {ip: '127.0.0.1', port: 7116};
 
 // const n1 = {ip: '3.144.233.59', port: 1234}; // 1
 // const n2 = {ip: '3.149.2.144', port: 1234}; // 2
 // const n3 = {ip: '18.188.59.235', port: 1234}; // 3
+
+const n1 = {ip: '54.234.21.159', port: 1234}
+const n2 = {ip: '52.2.162.238', port: 1234}
+const n3 = {ip: '34.233.122.175', port: 1234}
 
 
 
@@ -123,62 +127,62 @@ beforeAll((done) => {
     crawlGroup[id.getSID(n1)] = n1;
     crawlGroup[id.getSID(n2)] = n2;
     crawlGroup[id.getSID(n3)] = n3;
-    crawlGroup[id.getSID(n4)] = n4;
-    crawlGroup[id.getSID(n5)] = n5;
-    crawlGroup[id.getSID(n6)] = n6;
+    // crawlGroup[id.getSID(n4)] = n4;
+    // crawlGroup[id.getSID(n5)] = n5;
+    // crawlGroup[id.getSID(n6)] = n6;
 
     fs.writeFileSync("visited.txt", "\n");
     const startNodes = (cb) => {
-      distribution.local.status.spawn(n1, (e, v) => {
-        distribution.local.status.spawn(n2, (e, v) => {
-          distribution.local.status.spawn(n3, (e, v) => {
-            distribution.local.status.spawn(n4, (e, v) => {
-              distribution.local.status.spawn(n5, (e, v) => {
-                distribution.local.status.spawn(n6, (e, v) => {
-                  cb();
-                });
-              });
-            });
-          });
-        });
-      });
+      // distribution.local.status.spawn(n1, (e, v) => {
+      //   distribution.local.status.spawn(n2, (e, v) => {
+      //     distribution.local.status.spawn(n3, (e, v) => {
+            // distribution.local.status.spawn(n4, (e, v) => {
+            //   distribution.local.status.spawn(n5, (e, v) => {
+            //     distribution.local.status.spawn(n6, (e, v) => {
+                cb();
+            //     });
+            //   });
+            // });
+      //     });
+      //   });
+      // });
     };
   
     distribution.node.start((server) => {
       localServer = server;
   
 
-      startNodes(() => {
+      // startNodes(() => {
         const crawlConfig = {gid: 'crawl'};
         distribution.local.groups.put(crawlConfig, crawlGroup, (e, v) => {
           distribution.crawl.groups.put(crawlConfig, crawlGroup, (e, v) => {
             done();
           });
         });
-      });
+      // });
     });
   });
   
 afterAll((done) => {
-  const remote = {service: 'status', method: 'stop'};
-  remote.node = n1;
-  distribution.local.comm.send([], remote, (e, v) => {
-    remote.node = n2;
-    distribution.local.comm.send([], remote, (e, v) => {
-      remote.node = n3;
-      distribution.local.comm.send([], remote, (e, v) => {
-        remote.node = n4;
-        distribution.local.comm.send([], remote, (e, v) => {
-          remote.node = n5;
-          distribution.local.comm.send([], remote, (e, v) => {
-            remote.node = n6;
-            distribution.local.comm.send([], remote, (e, v) => {
+  // const remote = {service: 'status', method: 'stop'};
+  // remote.node = n1;
+  // distribution.local.comm.send([], remote, (e, v) => {
+  //   remote.node = n2;
+  //   distribution.local.comm.send([], remote, (e, v) => {
+  //     remote.node = n3;
+  //     distribution.local.comm.send([], remote, (e, v) => {
+  //       remote.node = n4;
+  //       distribution.local.comm.send([], remote, (e, v) => {
+  //         remote.node = n5;
+  //         distribution.local.comm.send([], remote, (e, v) => {
+  //           remote.node = n6;
+  //           distribution.local.comm.send([], remote, (e, v) => {
               localServer.close();
               done();
-            });
-          });
-        });
-      });
-    });
-  });
+  //           });
+  //         });
+  //       });
+  //     });
+  //   });
+  // });
 });
